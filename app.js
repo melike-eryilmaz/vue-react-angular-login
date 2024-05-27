ae5392e73d3fdac74e40910f2db821cd3a2d6aa6
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2024-05-27 10:27:15 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+import Login from './Login';
+export default function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
